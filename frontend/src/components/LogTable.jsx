5525d1c14e1ce1c3bfb9bedb68547160e15c5423
David Hernandez <davidmosquera@uninorte.edu.co>
2025-05-20 21:44:31 -0500

    logs filter by date

--- frontend/src/components/LogTable.jsx
+++ frontend/src/components/LogTable.jsx
@@ -3,13 +3,26 @@ import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import LogDropdown from "./LogDropdown";
 import { fetchLogsFilter, fetchLogs } from "../api/logsApi";
+import { Calendar } from "primereact/calendar";
 function LogTable() {
   const [view, setView] = useState("none");
   const [showTables, setShowTables] = useState(false);
   const [logEndpoint, setLogEndpoint] = useState("none");
   const [params, setParams] = useState({});
-  const [logs, setLogs] = useState({});
-  console.log(`view: ${view}. endpoint ${logEndpoint}`);
+  const [logs, setLogs] = useState([]);
+
+  const [date, setDate] = useState(null);
+  //console.log(`view: ${view}. endpoint ${logEndpoint}`);
+
+  function updateDate(e) {
+    setDate(e.value);
+    const selectedDate = e.value.toISOString().split("T")[0];
+    console.log(selectedDate);
+    const logsFiltered = logs.filter((log) =>
+      log.timestamp.includes(selectedDate)
+    );
+    setLogs(logsFiltered);
+  }
 
   useEffect(() => {
     if (showTables && logEndpoint !== "none") {
@@ -30,6 +43,9 @@ function LogTable() {
         setParams={setParams}
         setShowTables={setShowTables}
       />
+
+      <Calendar value={date} onChange={(e) => updateDate(e)} showIcon />
+
       {params.type_filter == "request" && (
         <>
           <DataTable value={logs} tableStyle={{ minWidth: "50rem" }}>
